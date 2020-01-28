ae497564d1d45dba7f69b2cea2a3d63da994d07c
Mish Mercer <mishell.mercer@gmail.com>
2020-01-27 18:09:58 -0800

    one more time.......

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="NavBar">
+        <h1>UFO Sightings by State</h1>
+        <a href="#" className="NavBtn">Home</a>
+        <a href="#" className="NavBtn">About</a>
+      </div>
+
+      <div className="StateCheckboxContainer">
+      </div>
+
+      <div className="graphContainer">
+      </div>
+    </div>
+  );
+}
+
+export default App;
